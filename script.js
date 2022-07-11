6f634ad4d0a5e75bafb9dc30f63c6788704df809
Tiffany DURR <103035560+TiffanyDURR@users.noreply.github.com>
2022-07-11 02:57:30 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-let score = 4000;
+let score = 3000;
 let compteurBonusPattoune = 0;
 let pattouneBuilding = 0;
-let buildings;
-let costPattoune = 100 * 1.5 * compteurBonusPattoune * pattouneBuilding;
+let costPattoune = 100 * 1.15 ** (compteurBonusPattoune);
 const affichageScore = document.querySelector(".affichageScore");
 const chatACliquer1 = document.querySelector(".chatACliquer1");
 const clicPlusUnImage = document.querySelector(".clicPlusUnImage");
 const superPattouneBonus = document.querySelector(".superPattouneBonus");
-const pattouneHeader = document.querySelector(".pattouneHeader");
+const pattouneHeader = document.querySelector(".pattouneHeader > div");
 
 
 // AU CLIC 
@@ -32,38 +31,36 @@ setTimeout(function () {
 function superPattouneFunction () {
   superPattouneBonus.addEventListener("click", () => {
     compteurBonusPattoune++;
-    pattouneBuilding = 1;
-    costPattoune = 100 * 1.5 * compteurBonusPattoune * pattouneBuilding;
-    console.log("pattouneBuilding " + pattouneBuilding);
-    console.log("Compteur bonus Pattoune " + compteurBonusPattoune);
-    setInterval(superPattouneCalc, 5000);
-
-    console.log(costPattoune);
+    costPattoune = 100 * 1.15 ** (compteurBonusPattoune);
+    costPattoune = Math.ceil(costPattoune);
     score = score - costPattoune;
+    score = Math.ceil(score);
+    setInterval(superPattouneCalc, 10000);
   })
   }
 
   function superPattouneCalc () {
-    score = score + compteurBonusPattoune * 1;
+    score = score + (1 * compteurBonusPattoune / compteurBonusPattoune);
   }
 
 
-
-
 setInterval(() => {
-  pattouneHeader.textContent = `Nombre de pattounes achetées ${compteurBonusPattoune} prochain cout de pattoune ${costPattoune + 150}`;
+  let costPattouneArrondi = costPattoune * 1.15;
+  costPattouneArrondi = Math.ceil(costPattouneArrondi);
+  if (compteurBonusPattoune == 0) {
+    pattouneHeader.style.display = "none";
+  } if (compteurBonusPattoune >= 1) {
+    pattouneHeader.style.display = "block";
+  }
+  pattouneHeader.innerHTML = `Nombre de pattounes achetées <span>${compteurBonusPattoune}</span> prochain cout de pattoune ${costPattouneArrondi}
+  <br/>
+  Ce bonus rapporte ${compteurBonusPattoune / 10} chat(s) toutes les secondes !`;
   affichageScore.textContent = `${score}`;
-
-  function affichageSuperPattouneBonus () {
-    if (score <= costPattoune + 150) {
-      console.log("costPattoune " + costPattoune);
+    if (score <= costPattouneArrondi) {
       superPattouneBonus.style.display = "none";
     } else {
       superPattouneBonus.style.display = "block";
     }
-  };
-
-  affichageSuperPattouneBonus();
 }, 1)
 
 
